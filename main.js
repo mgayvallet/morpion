0e7f89c8b80594b45b397bf695d327343d47f9ae
mathgvlt0 <mgayvallet@edenschool.fr>
2024-03-28 15:58:33 +0100

    added player2

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-let currentPlayer = "x"
+const players = [1, 2];
+let currentIndex = 0;
 
-const squares = document.querySelectorAll(".cell")
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', ()=>{
-        squares[i].textContent = currentPlayer
-    })
+function getNextPlayer() {
+  const player = players[currentIndex];
+  currentIndex = 1 - currentIndex; 
+  return player;
 }
+
+const squares = document.querySelectorAll(".cell");
+squares.forEach(square => {
+    square.addEventListener('click', () => {
+        if (square.textContent === '') {
+            const player = getNextPlayer();
+            if (player === 1) {
+                square.textContent = "X";
+            } else if (player === 2) {
+                square.textContent = "O";
+            }
+        }
+    });
+});
